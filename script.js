363fbd6c8cebd170c7cb646b8945d379c399a4d1
adrian <profilte82@gmail.com>
2025-11-10 12:46:34 +0000

    cambios en el javascript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const botones = document.querySelectorAll('.button-apply-job');
+// Devuelve una NodeList con todos los botones de aplicar empleo
+/*const botones = document.querySelectorAll('.button-apply-job');
 
 botones.forEach(boton => {
     boton.addEventListener('click', () => {
@@ -6,4 +7,15 @@ botones.forEach(boton => {
         boton.textContent = '¡Aplicado!';
         boton.disabled = true;
     });
+});*/
+
+// Manera correcta de hacerlo con delegación de eventos
+document.addEventListener('click', (event) => {
+    // Verifica si el elemento clicado es un botón de aplicar empleo
+    if (event.target.classList.contains('button-apply-job')) {
+        const boton = event.target;
+        boton.classList.add('is-applied');
+        boton.textContent = '¡Aplicado!';
+        boton.disabled = true;
+    }
 });

2f00cc753344c082e27c3cdad8e1ec86172b8f11
adrian <profilte82@gmail.com>
2025-11-24 12:52:47 +0000

    recuperación de los datos de data.json

--- script.js
+++ script.js
@@ -32,10 +32,20 @@ document.addEventListener('click', (event) => {
 });
 
 const filter = document.querySelector('#filter-location');
-const jobs = document.querySelectorAll('.job-listing-card');
+const mensaje = document.querySelector('#filter-selected-value');
 
 filter.addEventListener('change', () => {
+
+    const jobs = document.querySelectorAll('.job-listing-card');
+
     const selectedLocation = filter.value;
+
+    /*if (selectedLocation) {
+        mensaje.textContent = `Has seleccionado: ${selectedLocation}`;
+    } else {
+        mensaje.textContent = '';
+    }*/
+
     jobs.forEach(job => {
         const jobLocation = job.getAttribute('data-modalidad');
         const isShown = selectedLocation === '' || jobLocation === selectedLocation;
@@ -44,8 +54,31 @@ filter.addEventListener('change', () => {
 });
 
 
-fetch(./data.json)
+const container = document.querySelector('.jobs-listings');
+
+fetch("./data.json")
     .then((response) => {
         return response.json();
+    })
+    .then((jobs) => {
+        jobs.forEach(job => {
+
+        const article = document.createElement('article');
+        article.className = 'job-listing-card';
+
+        article.dataset.modalidad = job.data.modalidad;
+        article.dataset.nivel = job.data.nivel;
+        article.dataset.tecnology = job.data.tecnology;
+
+        article.innerHTML = `
+            <div>
+                <h3>${job.titulo}</h3>
+                <small>${job.empresa} | ${job.ubicacion}</small>
+                <p>${job.descripcion}</p>
+            </div>
+            <button class="apply-job">Aplicar</button>`
+            container.appendChild(article);
+        });
+
     })
 
